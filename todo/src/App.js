5b133b89b2c3d3d1cad46dd280143a66162e26a8
marleigh <volcana@gmail.com>
2021-07-12 10:19:47 -0700

    initial commit on the to-do list with react

---
+++ todo/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Test
+    </div>
+  );
+}
+
+export default App;
+
+// From scratch, initialize the React app
+// Render an <App /> component
+// Create the <App /> component from scratch
+// Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
+// like you're making a todo list with some hard-coded items on it
